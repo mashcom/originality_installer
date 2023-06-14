9c05660b0a7ed7890f8cc49e2e45fa3d141c6a1f
Blessing Mashoko <bmashcom@hotmail.com>
2023-06-14 17:15:59 +0200

    intial commit, good luck :-)

--- core_installer.sh
+++ core_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+#set -eu
 
 # Update package lists
 sudo apt-get update
@@ -39,6 +39,8 @@ sudo apt-get install mariadb-server
 # Start MariaDB service
 sudo systemctl start mariadb
 
+
+
 # Secure MariaDB installation
 sudo mysql_secure_installation
 
@@ -49,3 +51,98 @@ sudo mysql_secure_installation
 #FLUSH PRIVILEGES;
 #EOF
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+#-----------------------------------------------------------------------------------------------
+#
+# APPLICATION INSTALLER
+#
+#-----------------------------------------------------------------------------------------------
+
+
+## SCRIPT INSTALLATION
+#enable apache mod rewrite
+sudo a2enmod rewrite
+
+#install python-venv
+sudo apt install python3.10-venv
+
+
+#remove previous installations
+sudo rm -r originality-integration-fiverr
+
+
+# Clone the repository from GitHub
+sudo git clone https://github.com/mashcom/originality-integration-fiverr
+
+# Navigate to the cloned repository
+cd originality-integration-fiverr
+
+#create the .env file
+sudo cp .env_example .env
+
+# Create a directory for virtual environments
+sudo mkdir .venvs
+
+# Create a virtual environment for Django
+sudo python3 -m venv venvs/django
+
+# Activate the virtual environment
+source venvs/django/bin/activate
+
+# Create a log file for Django
+sudo touch logs/django.log
+
+# Set permissions for the log file
+sudo chmod 0777 logs/django.log
+
+# Change ownership of the repository to www-data user and group
+sudo chown -R www-data:www-data .
+
+# Set directory permissions
+sudo chmod 0777 .
+
+# Install Python dependencies
+sudo venvs/django/bin/pip install -r requirements.txt
+
+
+#-----------------------------------------------------------------------------------------------
+#
+# DATABASE CONFIGURATION
+#
+#-----------------------------------------------------------------------------------------------
+
+# Color codes for echo statements
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to echo colored text
+print_green() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+print_red() {
+    echo -e "${RED}$1${NC}"
+}
+
+# Confirm credentials
+print_green "Make sure you are able to log in using the command 'mariadb -u root -p'. This is to ensure that the application is able to read and write to the database"
+read -p "Did you confirm if the  the credentials in the .env file are correct?  (Y/N): " confirm
+if [[ "$confirm" != "Y" && "$confirm" != "y" ]]; then
+    print_red "Script stopped. Please update the credentials in the .env file.  "
+    exit 1
+fi
+
+# Run database migrations
+print_green "CREATING MIGRATION"
+sudo venvs/django/bin/python manage.py makemigrations
+
+print_green "RUNNING MIGRATIONS"
+sudo venvs/django/bin/python manage.py migrate
+
+print_green "PLEASE ENTER THE CREDENTIALS OF SUPER ADMIN USER"
+#create application super user
+sudo venvs/django/bin/python manage.py createsuperuser
