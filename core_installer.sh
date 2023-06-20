8f34ee1a8322969580c0f95c4b9e373a8c006226
Blessing Mashoko <bmashcom@hotmail.com>
2023-06-20 17:24:52 +0200

    updates

--- core_installer.sh
+++ core_installer.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 #set -eu
+# Color codes for echo statements
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to echo colored text
+print_green() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+print_red() {
+    echo -e "${RED}$1${NC}"
+}
 
 # Update package lists
 sudo apt-get update
@@ -15,6 +28,7 @@ sudo apt-get install libpq-dev              # PostgreSQL development headers
 sudo apt-get install netcat                 # Networking utility for reading from/writing to network connections
 sudo apt-get install default-libmysqlclient-dev   # MySQL development headers
 sudo apt-get install  libmagic-dev             # Development files for libmagic, a file type detection library
+sudo apt-get install expect
 
 # Remove cached package lists
 sudo rm -rf /var/lib/apt/lists/*
@@ -22,38 +36,52 @@ sudo rm -rf /var/lib/apt/lists/*
 # Update package lists again
 sudo apt-get update
 
-
 #install snapd
 sudo apt update
 sudo apt install snapd
 sudo snap install core; sudo snap refresh core
 
-#install certbot
+#install certbot, this is used for SSL certificate generation and management
 sudo apt-get remove certbot
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
+print_green "INSTALLING MARIADB"
 # Install MariaDB server
 sudo apt-get install mariadb-server
 
 # Start MariaDB service
 sudo systemctl start mariadb
 
+generate_password() {
+    local length=$1
+    local password=$(LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w "$length" | head -n 1)
+    echo "$password"
+}
 
+mariadb_password=$(generate_password 12)
+
+print_red "THE SCRIPT WANTS TO SECURE MARIADB PLEASE THE PASSWORD BELOW. CANNOT ADD IT MANUALLY. BELOW IS YOUR GENERATED PASSWORD. PLEASE SAVE IT SOMEWHERE AND ALWAYS USE IT WHEN ASKED FOR MARIAB PASSWORD DURING INSTALLATION THIS IS IMPORTANT!!!"
+print_green "$mariadb_password"
 
 # Secure MariaDB installation
 sudo mysql_secure_installation
 
-
+print_green "FLUSHING MARIADB PRIVILEGES. WHEN ASKED FOR PASSWORD USE THE ONE BELOW"
+print_green "$mariadb_password"
 #flush tables
-#sudo mariadb -u root -p <<EOF
-#GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
-#FLUSH PRIVILEGES;
-#EOF
+sudo mariadb  <<EOF
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$mariadb_password' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+print_green "CREATING INITIAL DATABASE. WHEN ASKED FOR PASSWORD USE THE ONE BELOW"
+print_green "$mariadb_password"
+sudo mariadb  <<EOF
+CREATE DATABASE IF NOT EXISTS originality_app;;
+EOF
+
+
 
 #-----------------------------------------------------------------------------------------------
 #
@@ -82,6 +110,9 @@ cd originality-integration-fiverr
 
 #create the .env file
 sudo cp .env_example .env
+
+#update .env database password
+sed -i "s/^DATABASE_PASSWORD=.*/DATABASE_PASSWORD=$mariadb_password/g" .env
 
 # Create a directory for virtual environments
 sudo mkdir .venvs
@@ -114,27 +145,7 @@ sudo venvs/django/bin/pip install -r requirements.txt
 #
 #-----------------------------------------------------------------------------------------------
 
-# Color codes for echo statements
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 
-# Function to echo colored text
-print_green() {
-    echo -e "${GREEN}$1${NC}"
-}
-
-print_red() {
-    echo -e "${RED}$1${NC}"
-}
-
-# Confirm credentials
-print_green "Make sure you are able to log in using the command 'mariadb -u root -p'. This is to ensure that the application is able to read and write to the database"
-read -p "Did you confirm if the  the credentials in the .env file are correct?  (Y/N): " confirm
-if [[ "$confirm" != "Y" && "$confirm" != "y" ]]; then
-    print_red "Script stopped. Please update the credentials in the .env file.  "
-    exit 1
-fi
 
 # Run database migrations
 print_green "CREATING MIGRATION"
@@ -143,6 +154,10 @@ sudo venvs/django/bin/python manage.py makemigrations
 print_green "RUNNING MIGRATIONS"
 sudo venvs/django/bin/python manage.py migrate
 
-print_green "PLEASE ENTER THE CREDENTIALS OF SUPER ADMIN USER"
+print_green "INSTALLATION COMPLETED BELOW IS YOUR MARIADB PASSWORD"
+
+print_green $mariadb_password
+
+#print_green "PLEASE ENTER THE CREDENTIALS OF SUPER ADMIN USER"
 #create application super user
-sudo venvs/django/bin/python manage.py createsuperuser
+#sudo venvs/django/bin/python manage.py createsuperuser
